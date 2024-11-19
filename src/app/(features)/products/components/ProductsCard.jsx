a5aa5f2a94c76b14363675e22acf7ec0c713d491
Picasso Gabriel <gpicasso25@gmail.com>
2024-11-19 19:42:17 -0300

    Cards con sus estilos

--- src/app/(features)/products/components/ProductsCard.jsx
+++ src/app/(features)/products/components/ProductsCard.jsx
@@ -1,25 +1,21 @@
 "use client";
-
+import styles from "./ProductsCard.module.css";
 const ProductsCard = ({ title, description, price, image }) => {
-
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <img className="rounded-t-lg" src={image} alt={title} />
-      <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
-        <p className="mb-3 font-bold text-gray-900 dark:text-white">{price}</p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
-        >
-          Comprar ahora
-        </a>
+    <div className={styles.cardContainer}>
+      <img className={styles.cardImg} src={image} alt={title} />
+      <div className={styles.infoContainer}>
+        <h5 className={styles.cardTitle}>{title}</h5>
+        <p className={styles.cardInfo}>{description}</p>
+        <div className={styles.priceAndButton}>
+          <p className={styles.cardPrice}>${price}</p>
+          <a href="#" className={styles.cardButton}>
+            Comprar
+          </a>
+        </div>
       </div>
     </div>
   );
-}
+};
 
-export default ProductsCard
-
-
+export default ProductsCard;
