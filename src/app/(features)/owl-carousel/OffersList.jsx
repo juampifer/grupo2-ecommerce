0f11cf8f26fedfa4d9d47fd36ee8bbc8f38403b6
Juan Pablo Fernandez <juampifer@gmail.com>
2024-12-03 11:16:41 -0300

    Comprar oferta por owl carousel, fix en json de productos

--- src/app/(features)/owl-carousel/OffersList.jsx
+++ src/app/(features)/owl-carousel/OffersList.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import OffersCard from "./OffersCard";
 
-const OffersList = ({ offers }) => {
+const OffersList = ({ offers, items }) => {
   return (
     <div className="flex flex-wrap gap-4 justify-center">
       {offers.map((offer) => (
         <OffersCard
           key={offer.id}
+          id={offer.id}
           title={offer.title}
           description={offer.description}
           price={offer.price}
           offerPrice={offer.offerPrice}
           image={offer.image}
+          items={items}
         />
       ))}
     </div>
