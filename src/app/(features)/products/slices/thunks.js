43a7f5a47a89d56b33c1089e73cf69b81b4e1c69
Juan Pablo Fernandez <juampifer@gmail.com>
2024-12-31 08:18:27 -0300

    Cambios para usar json-server

--- src/app/(features)/products/slices/thunks.js
+++ src/app/(features)/products/slices/thunks.js
@@ -4,8 +4,8 @@ import { setProducts, startLoadingProducts, setError } from "./productSlice";
 export const fetchProductsThunk = (categoryId='') => async (dispatch) => {
     try {
         dispatch(startLoadingProducts());
-        const { data } = await fetchProducts(categoryId);
-        dispatch(setProducts(data));
+        const products = await fetchProducts(categoryId);
+        dispatch(setProducts(products));
     } catch (error) {
         dispatch(setError(error.message));
     }
